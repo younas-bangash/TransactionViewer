3451a6179feb86f6fe5dd2e33ae97c1b20aeaa03
younasbangash <younas.appcom@gmail.com>
2017-02-14 18:07:03 +0400

    Changes in TestClasses

--- app/src/main/java/com/example/transaction/model/Transaction.java
+++ app/src/main/java/com/example/transaction/model/Transaction.java
@@ -1,16 +1,13 @@
 package com.example.transaction.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class Transaction {
-
     @JsonProperty("amount")
     private String amount;
     @JsonProperty("sku")
     private String sku;
     @JsonProperty("currency")
     private String currency;
-
 
     public double getAmount() {
         return Double.parseDouble(amount);
